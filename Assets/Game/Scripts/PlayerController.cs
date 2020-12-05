2f3c0d58a384592f1d9fd91534b9e9a9de11fc6f
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2020-12-06 00:04:02 +0700

    + Jump and ground check

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -9,23 +9,52 @@ public class PlayerController : MonoBehaviour
 
     [Header("---Movements---")]
     public float m_MoveSpd;
+    public float m_GravityModifier;
+    public float m_JumpPower;
     public CharacterController m_CharCon;
     public Transform m_CamPointTf;
     private Vector3 m_MoveInput;
 
     [Header("---Sensivity---")]
     public float m_MouseSen;
-    // public bool m_InvertX;
-    // public bool m_InvertY;
+
+    [Header("---Jump---")]
+    public bool m_CanJump;
+    public Transform m_GroundCheckTf;
+    public LayerMask m_GroundLM;
 
     private void Update()
     {
+        float yStore = m_MoveInput.y;
+
         Vector3 vertMove = m_Tf.forward * Input.GetAxis("Vertical");
         Vector3 horiMove = m_Tf.right * Input.GetAxis("Horizontal");
 
         m_MoveInput = vertMove + horiMove;
         m_MoveInput.Normalize();
         m_MoveInput = m_MoveInput * m_MoveSpd * Time.deltaTime;
+
+        m_MoveInput.y = yStore;
+        // m_MoveInput.y += Physics.gravity.y * m_GravityModifier * Time.deltaTime;
+
+        if (m_CharCon.isGrounded)
+        {
+            m_MoveInput.y = Physics.gravity.y * m_GravityModifier * Time.deltaTime;
+        }
+        else
+        {
+            m_MoveInput.y += Physics.gravity.y * m_GravityModifier * Time.deltaTime;
+        }
+
+        m_CanJump = Physics.OverlapSphere(m_GroundCheckTf.position, 0.25f, m_GroundLM).Length > 0;
+
+        // if (m_CharCon.isGrounded)
+        // {
+        if (Input.GetKeyDown(KeyCode.Space) && m_CanJump)
+        {
+            m_MoveInput.y = m_JumpPower;
+        }
+        // }
 
         m_CharCon.Move(m_MoveInput);
 
