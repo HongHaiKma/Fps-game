60975aebc9dcb75bbba3b1e20f6a0861622ff4b8
HongHai96 <nguyenhonghailqd1996@gmail.com>
2020-12-08 18:01:39 +0700

    no message

---
+++ Assets/Test/TestNavMesh/PlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerTest : MonoBehaviour
+{
+    public NavMeshAgent m_Agent;
+    public GameObject m_Enemy;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Agent.SetDestination(m_Enemy.transform.position);
+            Debug.Log("Keyco");
+        }
+    }
+}
