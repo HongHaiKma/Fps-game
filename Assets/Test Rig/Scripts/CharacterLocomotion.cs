dbeea88b489bcff072700ca89a08bfab85c8f7d9
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2020-12-18 11:54:41 +0700

    no message

--- Assets/Test Rig/Scripts/CharacterLocomotion.cs
+++ Assets/Test Rig/Scripts/CharacterLocomotion.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterLocomotion : MonoBehaviour
 {
     Animator anim_Onwer;
     Vector2 v2_Input;
 
+    [Header("---Animation Rigging---")]
+    public Rig r_Aiming;
+
     // Start is called before the first frame update
     void Start()
     {
         anim_Onwer = GetComponent<Animator>();
+        // r_Aiming.weight = 0f;
     }
 
     // Update is called once per frame
