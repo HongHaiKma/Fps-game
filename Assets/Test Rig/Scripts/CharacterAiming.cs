2f9945eb0692ab2216951a79ffec82b16f307f00
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2020-12-21 14:20:32 +0700

    no message

--- Assets/Test Rig/Scripts/CharacterAiming.cs
+++ Assets/Test Rig/Scripts/CharacterAiming.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float m_TurnSpd = 15f;
     Camera cam_Main;
+    public Rig r_AimLayer;
 
-    // Start is called before the first frame update
+    [Header("Test")]
+    public Transform tf_Start;
+    public Transform tf_End;
+
     void Start()
     {
         cam_Main = Camera.main;
@@ -15,7 +20,12 @@ public class CharacterAiming : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        Debug.DrawLine(tf_Start.position, tf_End.position, Color.red);
+    }
+
+
     void FixedUpdate()
     {
         float yawCam = cam_Main.transform.rotation.eulerAngles.y;
