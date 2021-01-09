a3a0c90d995817ce0c3660dbc6e6bb7778f460d2
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-01-09 17:58:32 +0700

    no message

--- Assets/Test/TestBehaviourTree/Chaser.cs
+++ Assets/Test/TestBehaviourTree/Chaser.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Panda;
 
 public class Chaser : MonoBehaviour
 {
@@ -13,13 +14,25 @@ public class Chaser : MonoBehaviour
     {
         float distance = Helper.CalDistance(tf_Owner.position, tf_Target.position);
 
-        if (distance > nav_Agent.stoppingDistance)
-        {
-            nav_Agent.SetDestination(tf_Target.position);
-        }
-        else
-        {
-            Helper.DebugLog("Idle");
-        }
+        // if (distance > nav_Agent.stoppingDistance)
+        // {
+        //     nav_Agent.SetDestination(tf_Target.position);
+        // }
+        // else
+        // {
+        //     Helper.DebugLog("Idle");
+        // }
+    }
+
+    [Task]
+    public void OnChasing()
+    {
+        nav_Agent.SetDestination(tf_Target.position);
+    }
+
+    [Task]
+    public void OnIdling()
+    {
+        Helper.DebugLog("Idle");
     }
 }
