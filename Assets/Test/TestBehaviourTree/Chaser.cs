49a485d93f29957772c9167b631b2230cbeda208
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-01-10 18:29:37 +0700

    no message

--- Assets/Test/TestBehaviourTree/Chaser.cs
+++ Assets/Test/TestBehaviourTree/Chaser.cs
@@ -10,18 +10,12 @@ public class Chaser : MonoBehaviour
     public Transform tf_Owner;
     public Transform tf_Target;
 
-    private void Update()
+    [Task]
+    public bool IsInRange()
     {
         float distance = Helper.CalDistance(tf_Owner.position, tf_Target.position);
 
-        // if (distance > nav_Agent.stoppingDistance)
-        // {
-        //     nav_Agent.SetDestination(tf_Target.position);
-        // }
-        // else
-        // {
-        //     Helper.DebugLog("Idle");
-        // }
+        return (distance <= nav_Agent.stoppingDistance);
     }
 
     [Task]
